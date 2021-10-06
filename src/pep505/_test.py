027abd0fae4cd4e3bbf4096de7ec21c698206096
Doug Swarin <dswarin@gmail.com>
2021-10-05 19:53:15 -0500

    add more precedence tests

--- src/pep505/_test.py
+++ src/pep505/_test.py
@@ -27,3 +27,27 @@ class TestPep505(unittest.TestCase):
     def test_complex(self):
         self.assertTrue(None?.foo?[0]?.foo ?? None ?? True ?? False)
 
+    def test_precedence_add(self):
+        x = 2 + None ?? 2
+        self.assertEqual(x, 4)
+
+    def test_precedence_negative(self):
+        x = 2 + None ?? -2
+        self.assertEqual(x, 0)
+
+    def test_precedence_multiply(self):
+        x = 2 * None ?? 4
+        self.assertEqual(x, 8)
+
+    def test_precedence_power(self):
+        x = 2 ** None ?? 4
+        self.assertEqual(x, 16)
+
+    def test_precedence_chained(self):
+        x = 2 ** None ?? 4 + 16
+        self.assertEqual(x, 32)
+
+    def test_precedence_chained_powers(self):
+        x = 2 ** None ?? 3 ** 4
+        self.assertEqual(x, 2417851639229258349412352)
+
